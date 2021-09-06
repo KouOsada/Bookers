8bd6f197e5754d7913dac4cb306f1b8a45512ea2
KouOsada <kouosada.0930@gmail.com>
2021-09-06 11:02:31 +0000

    [Add]バリデーション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,7 @@
 class BooksController < ApplicationController
 
   def index
-    @books = Book.all
+    @books = Book.all.order(:id)
     @book = Book.new
   end
 
@@ -10,29 +10,30 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    
-    if book.save
+    @book = Book.new(book_params)
+
+    if @book.save
       flash[:success] = 'Book was successfully created'
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book)
+    else
+      render :index
     end
-    
   end
 
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
     book = Book.find(params[:id])
-    
+
     if book.update(book_params)
       flash[:success] = 'Book was successfully updated.'
       redirect_to book_path(book.id)
     end
-    
+
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
